2bdfd6374ebf2cdb0876ba20d845377d2469f271
git-shuvam <github.shuvam@gmail.com>
2021-11-05 00:49:17 +0530

    issue

--- app/database.py
+++ app/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-from .config import settings
+from app.config import settings
 
 # database_url = "postgresql://postgres:postgres@127.0.0.1:5432/api"
 SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.DB_USER}:{settings.DB_PASSWORD}@{settings.DB_HOST}:{settings.DB_PORT}/{settings.DB_NAME}"
